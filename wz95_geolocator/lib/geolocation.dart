2e1f07d470b830b4a41a04619f9197a755bbfe1b
nash-2023 <abdalla.fat7y@hotmail.com>
2022-10-29 10:11:35 +0200

    geoLoc configurations

--- wz95_geolocator/lib/geolocation.dart
+++ wz95_geolocator/lib/geolocation.dart
@@ -8,3 +8,29 @@
 
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
+
+class GeoLoc extends StatefulWidget {
+  const GeoLoc({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<GeoLoc> createState() => _GeoLocState();
+}
+
+class _GeoLocState extends State<GeoLoc> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Geolocator"),
+        ),
+      ),
+    );
+  }
+}
