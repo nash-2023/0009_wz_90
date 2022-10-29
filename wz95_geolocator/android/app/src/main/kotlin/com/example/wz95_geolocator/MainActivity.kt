308091546b32d60b2fdc5e999a68d7d4d10e242e
nash-2023 <abdalla.fat7y@hotmail.com>
2022-10-29 09:20:36 +0200

    create new project with awesome dialog package revised

---
+++ wz95_geolocator/android/app/src/main/kotlin/com/example/wz95_geolocator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wz95_geolocator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ wz95_geolocator/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:wz95_geolocator/awe_dial.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fancy Dialog Example',
+      theme: ThemeData.light(),
+      home: const AweDial(title: 'Fancy Dialog Example'),
+    );
+  }
+}
